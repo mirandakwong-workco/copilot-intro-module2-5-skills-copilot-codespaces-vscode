7b7dcda8a1406195fce5b566531e1956c1604006
Miranda Kwong <mmkwongdev@outlook.com>
2025-03-11 20:21:02 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server
+const express = require('express');
+const app = express();
+const comments = require('./comments.json');
+
+app.use(express.static('public'));
+
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
